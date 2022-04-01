6dc7dd4acc8afbfaa3b10fd23e8fd1d5c3bcc7c1
Victoria <32303351+VSacr1@users.noreply.github.com>
2022-04-01 10:33:15 +0100

    Updated

--- tests/assessment_test.py
+++ tests/assessment_test.py
@@ -10,33 +10,33 @@ def test_endsPy():
     
     
 def test_one():
-    assert python2.one(['apple', 'banana', 'orange', 'orange', 'apple', 'apple']) == {'apple':3, 'orange':2, 'banana':1}
-    assert python2.one(['tic', 'tac', 'toe']) == {'tic':1, 'tac':1, 'toe':1}
-    assert python2.one([]) == {}
-    assert python2.one(['bert']*1000) == {'bert':1000}
+    assert example.one(['apple', 'banana', 'orange', 'orange', 'apple', 'apple']) == {'apple':3, 'orange':2, 'banana':1}
+    assert example.one(['tic', 'tac', 'toe']) == {'tic':1, 'tac':1, 'toe':1}
+    assert example.one([]) == {}
+    assert example.one(['bert']*1000) == {'bert':1000}
 
 def test_two():
     for a, b in [ (5, 3), (3, 1.5), (-5, 7) , (0, 1) ]:
         for operator in ['+', '-', '*', '/']:
-            assert python2.two(a, b, operator) == eval(f'{a} {operator} {b}')
+            assert example.two(a, b, operator) == eval(f'{a} {operator} {b}')
 
 def test_three():
-    assert python2.three(7) == 4
-    assert python2.three(64) == 64
-    assert python2.three(32) == 25
-    assert python2.three(1) == python2.three(2) == 1
+    assert example.three(7) == 4
+    assert example.three(64) == 64
+    assert example.three(32) == 25
+    assert example.three(1) == example.three(2) == 1
 
 def test_four():
-    assert python2.four(32, 24) == 8
-    assert python2.four(18, 11) == 1
-    assert python2.four(10, 50) == 10
-    assert python2.four(1, 1) == 1
-    assert python2.four(2, 2048) == 2
+    assert example.four(32, 24) == 8
+    assert example.four(18, 11) == 1
+    assert example.four(10, 50) == 10
+    assert example.four(1, 1) == 1
+    assert example.four(2, 2048) == 2
 
 def test_five():
-    assert python2.five('wxyz') == 'vwxy'
-    assert python2.five('abc') == 'zab'
-    assert python2.five('aAbB') == 'zZaA'
-    assert python2.five('abcabcABCABC') == 'zabzabZABZAB'
-    assert python2.five('hello world') == 'gdkkn vnqkc'
-    assert python2.five('54321') == '54321'
+    assert example.five('wxyz') == 'vwxy'
+    assert example.five('abc') == 'zab'
+    assert example.five('aAbB') == 'zZaA'
+    assert example.five('abcabcABCABC') == 'zabzabZABZAB'
+    assert example.five('hello world') == 'gdkkn vnqkc'
+    assert example.five('54321') == '54321'
