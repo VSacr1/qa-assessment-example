83176d25e8246620194135b7f42b23ad584ca44a
VSacr1 <vjsacre@gmail.com>
2022-07-21 15:12:23 +0100

    This is a change to fix indent error

--- programs/example.py
+++ programs/example.py
@@ -1,21 +1,21 @@
-    # <QUESTION 1>
+# <QUESTION 1>
 
-    # Given a string, return the boolean True if it ends in "py", and False if not. Ignore Case.
+# Given a string, return the boolean True if it ends in "py", and False if not. Ignore Case.
 
-    # <EXAMPLES>
+# <EXAMPLES>
 
-    # endsPy("ilovepy") → True
-    # endsPy("welovepy") → True
-    # endsPy("welovepyforreal") → False
-    # endsPy("pyiscool") → False
+# endsPy("ilovepy") → True
+# endsPy("welovepy") → True
+# endsPy("welovepyforreal") → False
+# endsPy("pyiscool") → False
 
-    # <HINT>
+# <HINT>
 
-    # What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
+# What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
     
     
-    def endsPy(input):
-        return ""
+def endsPy(input):
+    return ""
 
     
 # <QUESTION 2>
